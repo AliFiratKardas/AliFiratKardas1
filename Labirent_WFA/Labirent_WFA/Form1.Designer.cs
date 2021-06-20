0ac79f70239e4192af2452fdccfa21026da02c01
AliFiratKardas <afk.kardas21@gmail.com>
2021-06-20 14:08:33 -0700

    labirent oyunu
    ilk proje veya projecik

---
+++ Labirent_WFA/Labirent_WFA/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace Labirent_WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUst = new System.Windows.Forms.TextBox();
+            this.txtSOl = new System.Windows.Forms.TextBox();
+            this.txtYatay1 = new System.Windows.Forms.TextBox();
+            this.txtYatay2 = new System.Windows.Forms.TextBox();
+            this.txtSag = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.txtYatay4 = new System.Windows.Forms.TextBox();
+            this.txtYatay3 = new System.Windows.Forms.TextBox();
+            this.txt_finish = new System.Windows.Forms.TextBox();
+            this.txtDikey1 = new System.Windows.Forms.TextBox();
+            this.txtDikey2 = new System.Windows.Forms.TextBox();
+            this.txtDikey3 = new System.Windows.Forms.TextBox();
+            this.lblOyuncu = new System.Windows.Forms.Label();
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.txtAsagi = new System.Windows.Forms.TextBox();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUst
+            // 
+            this.txtUst.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtUst, "txtUst");
+            this.txtUst.Name = "txtUst";
+            this.txtUst.MouseHover += new System.EventHandler(this.txtUst_MouseHover);
+            // 
+            // txtSOl
+            // 
+            this.txtSOl.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtSOl, "txtSOl");
+            this.txtSOl.Name = "txtSOl";
+            this.txtSOl.MouseHover += new System.EventHandler(this.txtSOl_MouseHover);
+            // 
+            // txtYatay1
+            // 
+            this.txtYatay1.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtYatay1, "txtYatay1");
+            this.txtYatay1.Name = "txtYatay1";
+            this.txtYatay1.MouseHover += new System.EventHandler(this.txtYatay1_MouseHover);
+            // 
+            // txtYatay2
+            // 
+            this.txtYatay2.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtYatay2, "txtYatay2");
+            this.txtYatay2.Name = "txtYatay2";
+            this.txtYatay2.MouseHover += new System.EventHandler(this.txtYatay2_MouseHover);
+            // 
+            // txtSag
+            // 
+            this.txtSag.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtSag, "txtSag");
+            this.txtSag.Name = "txtSag";
+            this.txtSag.MouseHover += new System.EventHandler(this.txtSag_MouseHover);
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(this.textBox8, "textBox8");
+            this.textBox8.Name = "textBox8";
+            // 
+            // txtYatay4
+            // 
+            this.txtYatay4.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtYatay4, "txtYatay4");
+            this.txtYatay4.Name = "txtYatay4";
+            this.txtYatay4.MouseHover += new System.EventHandler(this.txtYatay4_MouseHover);
+            // 
+            // txtYatay3
+            // 
+            this.txtYatay3.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtYatay3, "txtYatay3");
+            this.txtYatay3.Name = "txtYatay3";
+            this.txtYatay3.MouseHover += new System.EventHandler(this.txtYatay3_MouseHover);
+            // 
+            // txt_finish
+            // 
+            this.txt_finish.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.txt_finish, "txt_finish");
+            this.txt_finish.ForeColor = System.Drawing.Color.Red;
+            this.txt_finish.Name = "txt_finish";
+            // 
+            // txtDikey1
+            // 
+            this.txtDikey1.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtDikey1, "txtDikey1");
+            this.txtDikey1.Name = "txtDikey1";
+            this.txtDikey1.MouseHover += new System.EventHandler(this.txtDikey1_MouseHover);
+            // 
+            // txtDikey2
+            // 
+            this.txtDikey2.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtDikey2, "txtDikey2");
+            this.txtDikey2.Name = "txtDikey2";
+            this.txtDikey2.MouseHover += new System.EventHandler(this.txtDikey2_MouseHover);
+            // 
+            // txtDikey3
+            // 
+            this.txtDikey3.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtDikey3, "txtDikey3");
+            this.txtDikey3.Name = "txtDikey3";
+            this.txtDikey3.MouseHover += new System.EventHandler(this.txtDikey3_MouseHover);
+            // 
+            // lblOyuncu
+            // 
+            resources.ApplyResources(this.lblOyuncu, "lblOyuncu");
+            this.lblOyuncu.Name = "lblOyuncu";
+            this.lblOyuncu.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblPuan
+            // 
+            resources.ApplyResources(this.lblPuan, "lblPuan");
+            this.lblPuan.Name = "lblPuan";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Aqua;
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // txtAsagi
+            // 
+            this.txtAsagi.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.txtAsagi, "txtAsagi");
+            this.txtAsagi.Name = "txtAsagi";
+            this.txtAsagi.MouseHover += new System.EventHandler(this.txtAsagi_MouseHover);
+            // 
+            // lblSkor
+            // 
+            resources.ApplyResources(this.lblSkor, "lblSkor");
+            this.lblSkor.Name = "lblSkor";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.txtAsagi);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.lblOyuncu);
+            this.Controls.Add(this.txtDikey3);
+            this.Controls.Add(this.txtDikey2);
+            this.Controls.Add(this.txtDikey1);
+            this.Controls.Add(this.txt_finish);
+            this.Controls.Add(this.txtYatay3);
+            this.Controls.Add(this.txtYatay4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.txtSag);
+            this.Controls.Add(this.txtYatay2);
+            this.Controls.Add(this.txtYatay1);
+            this.Controls.Add(this.txtSOl);
+            this.Controls.Add(this.txtUst);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUst;
+        private System.Windows.Forms.TextBox txtSOl;
+        private System.Windows.Forms.TextBox txtYatay1;
+        private System.Windows.Forms.TextBox txtYatay2;
+        private System.Windows.Forms.TextBox txtSag;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtYatay4;
+        private System.Windows.Forms.TextBox txtYatay3;
+        private System.Windows.Forms.TextBox txt_finish;
+        private System.Windows.Forms.TextBox txtDikey1;
+        private System.Windows.Forms.TextBox txtDikey2;
+        private System.Windows.Forms.TextBox txtDikey3;
+        private System.Windows.Forms.Label lblOyuncu;
+        private System.Windows.Forms.Label lblPuan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox txtAsagi;
+        private System.Windows.Forms.Label lblSkor;
+    }
+}
+
---
+++ Labirent_WFA/Labirent_WFA/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labirent_WFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void txtAsagi_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+        }
+
+        private void txtYatay1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+        private void txtYatay2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+
+        }
+        
+
+        private void txtYatay3_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+
+        }
+
+        private void txtYatay4_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+
+        }
+
+        private void txtUst_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+        private void txtSOl_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+        private void txtSag_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+        private void txtDikey1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+        private void txtDikey2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+        private void txtDikey3_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("oyunu kaybettiniz");
+            lblSkor.Text = "0";
+
+        }
+
+
+        int skor;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int skor = 1;
+
+            lblSkor.Text = Convert.ToString(skor);
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            skor = skor + 1;
+            lblSkor.Text = Convert.ToString(skor);
+            MessageBox.Show("TEBRİKLER PRENSESİ KURTARDINIZ");
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
